2fc65b9a0c9f89e6162df4b21dcaefad6cc8f5ae
AbhijeetNatu <natuabhijeet@gmail.com>
2021-03-27 05:48:24 -0700

    Day 2: Added file to be consumed

--- producer_consumer/producer/main.c
+++ producer_consumer/producer/main.c
@@ -4,6 +4,16 @@
 int main(void)
 {
     fprintf(stdout, "In %s file, at %d line",__FILE__,__LINE__);
+    // Todo:
+
+
+    // Get file name
+
+    // Check if there exist a file
+
+    // open file -- ReadOnly
+
+    // Read x number bytes into a buffer
 
 
     fprintf(stdout,"\n\n");
